084777ec46c73e902fe1cc1e9b5562a48d4210c3
raozhiming <raozhiming@elastos.org>
2019-04-09 18:57:49 +0800

    add devicemotion qrscanner

--- epks/plugindemo/js/qrscanner.js
+++ epks/plugindemo/js/qrscanner.js
@@ -1,79 +1,182 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
-var scanInfo = document.getElementById("info");
+function showStatus(status) {
+    s = "authorized:" + status.authorized + '<br>' +
+        "denied:" + status.authorized + '<br>' +
+        "restricted:" + status.authorized + '<br>' +
+        "prepared:" + status.authorized + '<br>' +
+        "scanning:" + status.authorized + '<br>' +
+        "previewing:" + status.authorized + '<br>' +
+        "showing:" + status.authorized + '<br>' +
+        "lightEnabled:" + status.authorized + '<br>' +
+        "canOpenSettings:" + status.authorized + '<br>' +
+        "canEnableLight:" + status.authorized + '<br>' +
+        "currentCamera:" + status.authorized + '<br>';
+    console.log(s);
+}
 
 function onDone(err, status){
-  if (err) {
-   // here we can handle errors and clean up any loose ends.
-      console.error(err);
-  }
-  if (status.authorized) {
-    // W00t, you have camera access and the scanner is initialized.
-    // QRscanner.show() should feel very fast.
-      console.log("status.authorized");
-  } else if (status.denied) {
-   // The video preview will remain black, and scanning is disabled. We can
-   // try to ask the user to change their mind, but we'll have to send them
-   // to their device settings with `QRScanner.openSettings()`.
-     console.log("status.denied");
-  } else {
-    // we didn't get permission, but we didn't get permanently denied. (On
-    // Android, a denial isn't permanent unless the user checks the "Don't
-    // ask again" box.) We can ask again at the next relevant opportunity.
-      console.log("other error");
-  }
+    showStatus(status);
 }
 
 function startPrepare() {
+    console.log("startPrepare");
     QRScanner.prepare(onDone);
 }
 
 //拍照
 function startScan() {
-
+    console.log("startScan");
     var callback = function(err, contents){
         if(err){
-            console.error(err._message);
+            if(err.name === 'SCAN_CANCELED') {
+                console.error('The scan was canceled before a QR code was found.');
+            } else {
+                console.error(err._message);
+            }
         }
-        scanInfo.innerHTML = 'The QR Code contains: ' + contents;
-
-        // window.QRScanner.hide(function(status){
-        //     console.log(status);
-        // });
-
-        window.QRScanner.cancelScan(function(status){
-          console.log(status);
-        });
-    };
+        console.log('The QR Code contains: ' + contents);
+    }
 
     window.QRScanner.scan(callback);
+}
 
-    window.QRScanner.show();
+function show() {
+    console.log("show");
+    window.QRScanner.show(function(status){
+        showStatus(status);
+    });
+}
+
+
+function destroy() {
+    QRScanner.destroy(function(status){
+        showStatus(status);
+    });
+}
+
+
+function cancelScan() {
+    console.log("cancelScan");
+    window.QRScanner.cancelScan(function(status){
+        showStatus(status);
+    });
+}
+
+function hide() {
+    console.log("hide");
+    window.QRScanner.hide(function(status){
+        showStatus(status);
+    });
+}
+
+function enableLight() {
+    QRScanner.enableLight(function(err, status){
+        err && console.error(err);
+        showStatus(status);
+    });
+}
+
+function disableLight() {
+    QRScanner.disableLight(function(err, status){
+        err && console.error(err);
+        showStatus(status);
+    });
+}
+
+function frontCamera() {
+    QRScanner.useFrontCamera(function(err, status){
+        err && console.error(err);
+        showStatus(status);
+    });
+}
+
+function backCamera() {
+    QRScanner.useBackCamera(function(err, status){
+        err && console.error(err);
+        showStatus(status);
+    });
+}
+
+function pausePreview() {
+    QRScanner.pausePreview(function(status){
+        showStatus(status);
+    })
+}
+
+function resumePreview() {
+    QRScanner.resumePreview(function(status){
+        showStatus(status);
+    })
+}
+
+function openSetting() {
+    QRScanner.getStatus(function(status){
+        showStatus(status);
+        if(status.canOpenSettings){
+            if(confirm("Would you like to enable QR code scanning? You can allow camera access in your settings.")){
+              QRScanner.openSettings();
+            }
+        }
+    });
 }
 
 var app = {
     // Application Constructor
     initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+
         document.getElementById('btnScan').addEventListener('click', startScan);
         document.getElementById('btnPrepare').addEventListener('click', startPrepare);
+        document.getElementById('btnShow').addEventListener('click', show);
+        document.getElementById('btnHide').addEventListener('click', hide);
+        document.getElementById('btnCancel').addEventListener('click', cancelScan);
+        document.getElementById('btnDestroy').addEventListener('click', destroy);
+        document.getElementById('btnEnableLight').addEventListener('click', enableLight);
+        document.getElementById('btnDisableLight').addEventListener('click', disableLight);
+        document.getElementById('btnFront').addEventListener('click', frontCamera);
+        document.getElementById('btnBack').addEventListener('click', backCamera);
+        document.getElementById('btnPause').addEventListener('click', pausePreview);
+        document.getElementById('btnResume').addEventListener('click', resumePreview);
+        document.getElementById('btnSetting').addEventListener('click', openSetting);
     },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
 };
 
 app.initialize();
