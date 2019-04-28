2df65afc88e345ece39ee0242210f622bb56f827
raozhiming <raozhiming@elastos.org>
2019-04-28 11:30:02 +0800

    update plugindemo:

--- epks/cloneEpk/cloneDapp.sh
+++ epks/cloneEpk/cloneDapp.sh
@@ -3,9 +3,9 @@
 clone_clount=0
 # dapp_path=""
 
-# usage() {
-#     echo "cloneDapp.sh -c clone_clount -n dapp_path"
-# }
+usage() {
+    echo "cloneDapp.sh clone_clount"
+}
 
 # while getopts "c:n:" arg #选项后面的冒号表示该选项需要参数
 # do
@@ -29,30 +29,31 @@ clone_clount=0
 if [ $# -ge 1 ] ; then
     clone_clount=$1
 else
-    usage()
-    exit 1
+    clone_clount=1
 fi
 
 echo "clone clount:"$clone_clount
+
+epkname="plugindemo"
 
 modifyManifest() {
     echo "manifest"
     dapp_name=$1
     echo $dapp_name
-    sed -i '/"id"/c\  "id": "org.elastos.trinity.'$dapp_name'",' splashscreen/manifest.json
+    sed -i '/"id"/c\  "id": "org.elastos.trinity.'$dapp_name'",' ${epkname}/manifest.json
     # todo author:name
-    sed -i '/"name": /c\  "name": "'$dapp_name'",' splashscreen/manifest.json
+    sed -i '/"name": /c\  "name": "'$dapp_name'",' ${epkname}/manifest.json
 }
 
 packageEpk() {
     echo "packageEpk"
-    cd splashscreen
+    cd ${epkname}
     zip -r $1.epk *
     cd ..
-    mv splashscreen/$1.epk epk/
+    mv ${epkname}/$1.epk epk/
 }
 
 for (( i = 0; i < $clone_clount; i++ ));do
-    modifyManifest "mytest"$i
-    packageEpk "mytest"$i
+    modifyManifest ${epkname}$i
+    packageEpk ${epkname}$i
 done
